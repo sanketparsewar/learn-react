cb2dccba912951d5e8bc97e65f21ae811ee22765
sanketp2001 <sanket.p@angularminds.in>
2025-05-14 14:22:55 +0530

    removed unused files, created element using react

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -1,9 +1,15 @@
+import Test from "./test"
 
 
 function App() {
-  
+  const userName='Sanket'
   return (
-    <h1>Hello</h1>
+    <>
+    <Test/>
+    {/* here {userName} is an evaluated expresssion, means variable injection */}
+    {/* here in {} we cannot write any js expression eg.if */}
+    <h1>Hello {userName} </h1>  
+    </>
   )
 }
 
--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,9 +1,30 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react';
+
+
+//this is jsx so we have to use paranthesis
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank">Google</a>
+);
+
+//we create element using the react
+// here it has to pass the required parameters
+// fisrt tag is tagname
+// second is attributes as object ex. href ,target
+// third if the text that we want to display
+const recatElement=React.createElement(
+  'a',
+  {
+    href:"https://www.google.com" ,target:"_blank"
+  },
+  'click to visit google'
+
+)
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
+    // anotherElement   // here we are directly giving the html element and written directly as object
+    // recatElement  // this is the react way of creating element
 )
