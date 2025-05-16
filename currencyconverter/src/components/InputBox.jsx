26bb94c2c9e45a12fb312512bbba5ae05490d94e
sanketp2001 <sanket.p@angularminds.in>
2025-05-16 14:47:41 +0530

    updated

--- currencyconverter/src/components/InputBox.jsx
+++ currencyconverter/src/components/InputBox.jsx
@@ -8,10 +8,9 @@ function InputBox({
   onCurrencyChange, //currency change
   currencyOptions = [], //all the currencies
   selectCurrency = "usd", //the selected urrency
-  isReadonly=false
+  isReadonly = false,
 }) {
   const amountInputId = useId(); //this will give a unique id, use for accessing using tab
-
 
   return (
     <div className="">
@@ -19,35 +18,35 @@ function InputBox({
         {label}:
       </label>
       <div>
-      <input
-        id={amountInputId}
-        type="number"
-        value={amount}
-        readOnly={isReadonly}
-        // here when passing the number as value it may possible that it is passsed as string so make type conversion
-        onChange={(event) =>
-          onAmountChange && onAmountChange(Number(event.target.value))
-        } //if anyone have not passed value then it may crash
-        className="focus:outline-0 px-3 py-2 border border-gray-400 rounded-xl "
-        placeholder="Enter value"
-      />
-      <label className="mx-1">
-        <select
+        <input
+          id={amountInputId}
+          type="number"
+          value={amount}
+          readOnly={isReadonly}
+          // here when passing the number as value it may possible that it is passsed as string so make type conversion
           onChange={(event) =>
-            onCurrencyChange && onCurrencyChange(event.target.value)
-          } //this is string
-          className="cursor-pointer focus:outline-0 px-2 hover:bg-gray-200 rounded-md py-1"
-          value={selectCurrency}
-        >
-          {/* array of currencies */}
-          {/* to get good performance in loop pass the key (key can be id,index,or unique value) */}
-          {currencyOptions.map((c) => (
-            <option  key={c} value={c}>
-              {c}
-            </option>
-          ))}
-        </select>
-      </label>
+            onAmountChange && onAmountChange(Number(event.target.value))
+          } //if anyone have not passed value then it may crash
+          className="focus:outline-0 px-3 py-2 border border-gray-400 rounded-xl "
+          placeholder="Enter value"
+        />
+        <label className="mx-1">
+          <select
+            onChange={(event) =>
+              onCurrencyChange && onCurrencyChange(event.target.value)
+            } //this is string
+            className="cursor-pointer focus:outline-0 px-2 hover:bg-gray-200 rounded-md py-1"
+            value={selectCurrency}
+          >
+            {/* array of currencies */}
+            {/* to get good performance in loop pass the key (key can be id,index,or unique value) */}
+            {currencyOptions.map((c) => (
+              <option key={c} value={c}>
+                {c}
+              </option>
+            ))}
+          </select>
+        </label>
       </div>
     </div>
   );
--- currencyconverter/src/hooks/useCurrencyInfo.js
+++ currencyconverter/src/hooks/useCurrencyInfo.js
@@ -1,12 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function useCurrencyInfo(currency) {
-    const [data, setData] = useState({})
-    let res;
-    useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`).then((res) => res.json()).then(json => setData(json[currency]))
-    }, [currency])
-    return data
+  const [data, setData] = useState({});
+  let res;
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+  return data;
 }
 
 export default useCurrencyInfo;
